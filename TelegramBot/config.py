15c110eedb404a4dda2c47706ffa8d50e2651a4f
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2022-03-30 23:39:57 +0800

    Update config.py

--- TelegramBot/config.py
+++ TelegramBot/config.py
@@ -1,19 +1,8 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-
 from opencc import OpenCC
-# 官方的 OpenCC
-# cc1 = OpenCC('tw2sp.json')  # 繁转简
-# cc2 = OpenCC('s2twp.json')  # 簡轉繁
-
-# opencc-python-reimplemented
-cc1 = OpenCC('tw2sp')  # 繁转简
-cc2 = OpenCC('s2twp')  # 簡轉繁
-
-
-# 你的Telegram Bot Token
-# 获取方式 @BotFather
-BOT_TOKEN = ""
+cc1 = OpenCC('tw2sp.json')  # 繁转简
+cc2 = OpenCC('s2twp.json')  # 簡轉繁
 
 
 # 你的 Pixiv REFRESH_TOKEN
@@ -21,3 +10,16 @@ BOT_TOKEN = ""
 # https://github.com/upbit/pixivpy/issues/158#issuecomment-778919084
 REFRESH_TOKEN = "0zeYA-PllRYp1tfrsq_w3vHGU1rPy237JMf5oDt73c4"
 
+
+# 你的Telegram Bot Token
+# 获取方式 @BotFather
+BOT_TOKEN = ""
+
+
+webhook_listen = '0.0.0.0'
+webhook_port = 443
+webhook_url = "https://host:port/" + BOT_TOKEN
+
+# 生成私钥和证书 openssl req -newkey rsa:2048 -sha256 -nodes -keyout private.key -x509 -days 3650 -out cert.pem
+webhook_key = "private.key"
+webhook_cert = "cert.pem"
