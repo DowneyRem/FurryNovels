9038548aac5b570d6b22ee4e170689ccbea7fafe
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2022-05-25 12:51:08 +0800

    Update Webdav3.py

--- TelegramBot/Webdav3.py
+++ TelegramBot/Webdav3.py
@@ -2,7 +2,10 @@
 # -*- coding: UTF-8 -*-
 import os
 import time
+import logging
+
 from webdav3.client import Client
+from webdav3.exceptions import *
 
 from FileOperate import removeFile, timethis
 from config import webdavdict3 as webdavdict
@@ -10,9 +13,10 @@ from config import webdavdict3 as webdavdict
 
 # webdavdict = {
 # 	"jianguoyun": {
-# 		"baseurl": "https://dav.jianguoyun.com/dav/",
-# 		"username": "",  # 你的账号，支持多组
-# 		"password": ""   # 你的密码
+# 		'webdav_hostname': "https://dav.jianguoyun.com/dav/",
+# 		'webdav_login': "",     # 你的账号，支持多组
+# 		'webdav_password': "",  # 你的密码
+# 		'disable_check': True,  # 有的网盘不支持check功能
 # 	},
 # }
 
@@ -29,28 +33,32 @@ def monthNow():
 @timethis
 def upload(options, path):
 	client = Client(options)
+	
+	url = options.get("webdav_hostname").split("/")[2]
 	name = os.path.split(path)[1]
 	webdavPath = "兽人小说/{}/{}".format(monthNow(), name)
 	# print(webdavPath)
+	
 	try:
 		client.upload(webdavPath, path)
-		url = options.get("webdav_hostname")
-		url = url.split("/")[2]
-		print("{} 已上传至：{}".format(name, url))
-	except :
-		print("请检查网络或代理")
-	
-
+		print("【{}】已上传至：{}".format(name, url))
+	except (ResponseErrorCode, NoConnection) as e:
+		print("【{}】上传 {} 失败".format(name, url))
+		logging.warning(e)
+	except Exception as e:
+		logging.exception(e)
+		
+		
 # @timethis
 def uploadAll(path, delete=0):
 	# delete 不为0时，上传后删除源文件
-	webdavs =list(webdavdict.items())
-	print(list(webdavdict.keys()))
+	webdavs = list(webdavdict.items())
+	# print(list(webdavdict.keys()))
 	for webdav in webdavs:
 		# print(webdav[0])
 		options = webdav[1]
 		upload(options, path)
-		
+	
 	if delete != 0:
 		name = os.path.split(path)[1]
 		try:
@@ -58,7 +66,7 @@ def uploadAll(path, delete=0):
 			print("【{}】已经删除".format(name))
 		except IOError:
 			print("【{}】删除失败".format(name))
-		
-		
+
+
 if __name__ == '__main__':
 	pass
