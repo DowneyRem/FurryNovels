95cacb85eec81b052bff74eb1bdd1fd2c8485703
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2026-01-25 13:44:56 +0800

    删除无用文件

--- Archived/TelegramBotVer3/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-systemctl --user stop furrynovels
-cp ./furrynovels.service ~/.config/systemd/user/
-systemctl --user daemon-reload
-systemctl --user start furrynovels
--- Archived/TelegramBotVer4/launch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-python TelegramBot.py &
-caddy reverse-proxy --from :8880 --to :8080
